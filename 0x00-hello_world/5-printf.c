035877d1b75b8792f0022b73a040bfb5c77f8c1d
Siyabonga Khwela <61106889+chi3fmhlongo@users.noreply.github.com>
2023-07-16 17:26:41 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-/* Starting Main function*/
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message to the standard output.
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
     printf("with proper grammar, but the outcome is a piece of art,\n");
