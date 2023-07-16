36b747168c02e29c533e88fb0c7a868947482952
Siyabonga Khwela <61106889+chi3fmhlongo@users.noreply.github.com>
2023-07-16 16:41:17 +0200

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    putchar('0' + 0);
-    putchar('0' + 1);
-    putchar('0' + 2);
-    putchar('0' + 3);
-    putchar('0' + 4);
-    putchar('0' + 5);
-    putchar('0' + 6);
-    putchar('0' + 7);
-    putchar('0' + 8);
-    putchar('0' + 9);
+    int number;
+
+    for (number = 0; number <= 9; number++) {
+        putchar('0' + number);
+    }
 
     putchar('\n');
 
