42f965800849278d4352823d6b8cacf6bae2a816
Siyabonga Khwela <61106889+chi3fmhlongo@users.noreply.github.com>
2023-07-16 16:28:01 +0200

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,15 @@
 
 int main() {
     int n = 98;
+    const char* messages[] = { "negative", "zero", "positive" };
 
-    printf(n ," is positive");
-    printf("-", n,"is negative");
-    printf("0"," is zero");
-    printf("\n");
+    printf("%d is %s\n", n, messages[n < 0 ? 0 : (n > 0 ? 2 : 1)]);
+
+    n = -98;
+    printf("%d is %s\n", n, messages[n < 0 ? 0 : (n > 0 ? 2 : 1)]);
+
+    n = 0;
+    printf("%d is %s\n", n, messages[n < 0 ? 0 : (n > 0 ? 2 : 1)]);
 
     return 0;
 }
