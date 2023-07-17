333f7c9f194e74fffec9541871e54013f7921716
Siyabonga Khwela <61106889+chi3fmhlongo@users.noreply.github.com>
2023-07-17 08:49:51 +0200

    Create 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,14 +7,12 @@
 int main(void)
 {
     int i, j;
-
     for (i = 0; i <= 8; i++)
     {
         for (j = i + 1; j <= 9; j++)
         {
             putchar((i % 10) + '0');
             putchar((j % 10) + '0');
-
             if (i != 8 || j != 9)
             {
                 putchar(',');
@@ -22,7 +20,6 @@ int main(void)
             }
         }
     }
-
     putchar('\n');
     return (0);
 }
