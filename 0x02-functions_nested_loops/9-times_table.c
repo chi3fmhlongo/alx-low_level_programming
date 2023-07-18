d02a1486ec956eacf1efedee69ee2bc37f6ada28
Siyabonga Khwela <61106889+chi3fmhlongo@users.noreply.github.com>
2023-07-18 19:55:06 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,28 +5,27 @@
  */
 void times_table(void)
 {
-    int row, column, product;
+	int row, column, product;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (column = 0; column <= 9; column++)
-        {
-            product = row * column;
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = row * column;
 
-            if (column != 0)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
+			if (column != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 
-            if (product < 10)
-                _putchar(' ');
-            else
-                _putchar('0' + (product / 10));
+			if (product < 10)
+				_putchar(' ');
+			else
+				_putchar('0' + (product / 10));
 
-            _putchar('0' + (product % 10));
-
-        }
-        _putchar('\n');
-    }
+			_putchar('0' + (product % 10));
+		}
+		_putchar('\n');
+	}
 }
