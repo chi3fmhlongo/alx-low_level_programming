da3c9751f5647e82e3607b63911ffa24db630d02
Siyabonga Khwela <61106889+chi3fmhlongo@users.noreply.github.com>
2023-07-18 18:59:46 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
  * main - Entry point of the program
  *
@@ -8,6 +7,6 @@
  */
 int main(void)
 {
-    printf("_putchar\n");
-    return 0;
+	printf("_putchar\n");
+	return (0);
 }
