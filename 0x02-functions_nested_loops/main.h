e91a98aeff43f111854aceae4ec96c72ea3df32c
Siyabonga Khwela <61106889+chi3fmhlongo@users.noreply.github.com>
2023-07-18 18:57:29 +0200

    Create main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Function prototypes */
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+int divide(int a, int b);
+
+#endif /* MAIN_H */
