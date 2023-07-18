cb31bdf757b76407c18bc8ecbf41ae7cc27a301b
Siyabonga Khwela <61106889+chi3fmhlongo@users.noreply.github.com>
2023-07-18 19:22:33 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,6 +11,7 @@ int subtract(int a, int b);
 int multiply(int a, int b);
 int divide(int a, int b);
 void print_alphabet_x10(void);
+int _islower(int c);
 void print_alphabet(void);
 
 #endif /* MAIN_H */
