e1a1ce14f4f1f893f8ace9a0cdbb166d316f5c1a
Siyabonga Khwela <61106889+chi3fmhlongo@users.noreply.github.com>
2023-07-27 20:00:47 +0200

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@
  *char prototypes()
  */
 void reverse_array(int *a, int n);
+void print_unsigned_int(unsigned int n)
 void print_number(int n);
 void print_buffer(char *b, int size);
 int _strcmp(char *s1, char *s2);
