f376389087110078af3a16a89f3796fedc10a5d7
Siyabonga Khwela <61106889+chi3fmhlongo@users.noreply.github.com>
2023-07-27 19:52:18 +0200

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,41 +6,17 @@
  */
 void print_number(int n)
 {
-	int i, j, digit, digits, power;
-	unsigned int temp, numchar, number;
-
-	digit = 0;
 	if (n < 0)
 	{
 		_putchar('-');
-		temp = -n;
+		if (n < -9)
+			print_number(n / -10);
+		_putchar('0' - n % 10);
 	}
 	else
 	{
-		temp = n;
-	}
-
-	number = temp;
-
-	while (number >= 10)
-	{
-		number = number / 10;
-		digit++;
-	}
-	digits = digit + 1;
-	power = 1;
-	i = 1;
-
-	while (i < digits)
-	{
-		power = power * 10;
-		i++;
-	}
-	j = power;
-	while (j >= 1)
-	{
-		numchar = (temp / j) % 10;
-		_putchar(numchar + '0');
-		j = j / 10;
+		if (n > 9)
+			print_number(n / 10);
+		_putchar(n % 10 + '0');
 	}
 }
