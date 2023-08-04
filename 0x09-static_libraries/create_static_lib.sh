6d00ff16c8b6ca8514dd9d5f1715d6e1c1dddba3
Siyabonga Khwela <61106889+chi3fmhlongo@users.noreply.github.com>
2023-08-04 15:59:32 +0200

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-gcc -c ./*.c
-ar -rc liball.a ./*.o
+# Compile all .c files into .o files
+gcc -c *.c
+# Create the static library liball.a from the .o files
+ar rcs liball.a *.o
+# Clean up the .o files
+rm *.o
